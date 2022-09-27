8e78a11dea1cda52027d2df97aa0e298ed8b18a5
Eden Avitan <eden.avitan@sap.com>
2022-09-27 23:02:49 +0300

    [INFRA] modified project structure

--- data.py
+++ data.py
@@ -3,25 +3,25 @@ import json
 from pymongo import MongoClient
 from pymongo.collection import Collection, ReturnDocument
 
-data1 = pd.read_csv("S10_question_answer_pairs.txt", sep="\t").dropna()
-data2 = pd.read_csv("S10_question_answer_pairs.txt", sep="\t").dropna()
-data3 = pd.read_csv("S09_question_answer_pairs.txt", sep="\t").dropna()
+data1 = pd.read_csv("qa/S10_question_answer_pairs.txt", sep="\t").dropna()
+data2 = pd.read_csv("qa/S10_question_answer_pairs.txt", sep="\t").dropna()
+data3 = pd.read_csv("qa/S09_question_answer_pairs.txt", sep="\t").dropna()
 
 concat_data = pd.concat([data1, data2, data3], axis=0)
 data = concat_data.drop(columns=['ArticleTitle', 'DifficultyFromQuestioner', 'DifficultyFromAnswerer', 'ArticleFile'])
 
-dict = data.groupby(['Question'], dropna=True).apply(lambda x: list(set(x['Answer'].tolist()))).to_dict()
-print(dict)
+data_dict = data.groupby(['Question'], dropna=True).apply(lambda x: list(set(x['Answer'].tolist()))).to_dict()
+print(data_dict)
 
 item_list = []
-for key, value in dict.items():
+for key, value in data_dict.items():
     temp_dict = {}
     temp_dict["Question"] = key
     temp_dict["Answers"] = value
 
     item_list.append(temp_dict)
 
-with open('data.json', 'w') as f:
+with open('db_data_copy.json', 'w') as f:
     json.dump(item_list, f)
 
 
@@ -30,6 +30,7 @@ def get_and_increment_next_qa_index(counter_collection: Collection):
     return counter_collection.find_one_and_update({'_id': "qa_id"},
                                                  {'$inc': {"counter": 1}},
                                                  return_document=ReturnDocument.AFTER)["counter"]
+
 
 URI = f"mongodb://localhost:27023"
 client = MongoClient(URI)
