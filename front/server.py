8147f629de9edaebb5fc206448265d723571724a
Tomer Efroni <t0merx.efr@gmail.com>
2022-10-01 14:52:44 +0300

    Added more front mechanic.

--- front/server.py
+++ front/server.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, render_template
+import os
+from flask import Flask, request, render_template, redirect, url_for, session
 import sys
 
 # sys.path.insert(0, os.path.abspath('..'))
@@ -6,12 +7,44 @@ sys.path.insert(0, os.getcwd())
 import consts
 
 app = Flask(__name__)
+app.secret_key = "justforproject"
 
 
-@app.route("/")
-@app.route("/login")
+@app.route("/", methods=["POST", "GET"])
+@app.route("/login", methods=["POST", "GET"])
 def login():
-    return render_template('login.html')
+    if request.method == "POST":
+        usr = request.form["usrname"]
+        password = request.form["pwd"]
+        # TODO: Add checking credentials mechanism, obviously, will hash password on registration and checking.
+
+        session["user"] = usr  # actually in cookies
+        return redirect(url_for("platform"))
+    else:
+        if "user" in session:
+            return redirect(url_for("platform"))
+        return render_template('login.html')
+
+
+@app.route("/register")
+def register():
+    # TODO: recheck both passwords are equal on registration
+    return render_template('register.html')
+
+
+@app.route("/platform")
+def platform():
+    if "user" in session:
+        usr = session["user"]
+        return render_template('platform.html', user=usr)
+
+    return redirect(url_for("login"))
+
+
+@app.route("/logout")
+def logout():
+    session.pop("user", None)
+    return redirect(url_for("login"))
 
 
 if __name__ == '__main__':
