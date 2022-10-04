920649f73fe6c7e0a411f43e522055f9d59ba4d1
Tomer Efroni <t0merx.efr@gmail.com>
2022-10-04 20:52:21 +0300

    Updated registration.

--- front/server.py
+++ front/server.py
@@ -1,6 +1,11 @@
-import os
 from flask import Flask, request, render_template, redirect, url_for, session
+from kazoo.protocol.states import EventType, WatchedEvent
+from kazoo.client import KazooClient, KazooState
+import os
 import sys
+import hashlib
+import requests
+import json
 
 # sys.path.insert(0, os.path.abspath('..'))
 sys.path.insert(0, os.getcwd())
@@ -8,6 +13,25 @@ import consts
 
 app = Flask(__name__)
 app.secret_key = "justforproject"
+
+
+def get_leader(zk, nodes_path):
+    children = zk.get_children(nodes_path)
+    for child in children:
+        address, job, status = zk.get(f"{nodes_path}/{child}")[0].decode().split(",")
+        if job == consts.LEADER:
+            return child, address, status
+    return None
+
+
+@staticmethod
+def connection_status_listener(state):
+    if state == KazooState.LOST:
+        print('session to zookeeper was lost')  # Register somewhere that the session was lost
+    elif state == KazooState.SUSPENDED:
+        print('disconnected from zookeeper')  # Handle being disconnected from Zookeeper
+    else:
+        print('connected to zookeeper')  # Handle being connected/reconnected to Zookeeper
 
 
 @app.route("/", methods=["POST", "GET"])
@@ -26,12 +50,25 @@ def login():
         return render_template('login.html')
 
 
-@app.route("/register")
+@app.route("/register", methods=["POST", "GET"])
 def register():
     # TODO: recheck both passwords are equal on registration
-    usr = request.form["usrname"]
-    password = request.form["pwd"]
-    repassword = request.form["repwd"]
+    if request.method == "POST":
+        usr = request.form["usrname"]
+        password = request.form["pwd"]
+        repassword = request.form["repwd"]
+        if password == repassword:
+            hashed_password = hashlib.sha256(password.encode()).hexdigest()
+            body = {"username": usr, "password": hashed_password}
+
+            leader = get_leader(zk, consts.BASE_PATH)
+            leader_domain = leader[1]
+
+            url = f"http://{leader_domain}/get_qa_query"
+            response = requests.post(url=url, json=json.dumps(body))
+            print(response.content)
+            return redirect(url_for("login"))
+
     return render_template('register.html')
 
 
@@ -51,5 +88,8 @@ def logout():
 
 
 if __name__ == '__main__':
+    zk = KazooClient(hosts='localhost:2181')
+    zk.start()
+    zk.add_listener(connection_status_listener)  # notify about connection change
     app.run(debug=True, use_reloader=False)
 
